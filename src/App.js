6a7f37eab13cddf15936aeb35dffe78b8946b823
Filszu <filipszumowski05@gmail.com>
2022-12-11 19:24:49 +0100

    finished website

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 import { ProgramCard } from './components';
 
 import { programs } from './constants/programs';
+
+import { useState } from 'react';
 function App() {
+
+  const [playerName, setPlayerName] = useState("")
   
 
 
@@ -18,11 +22,20 @@ function App() {
         BUDDY POINTS
         
      </header>
-     <div className="programmsContainer">
+     <section className="app__playerName">
+      {playerName&& <div>your playerID: {playerName}__{Date.now()}</div>}
+      <input type="text" onChange={(e)=>{setPlayerName(e.target.value)}} value={playerName} placeholder="your name"/>
+     </section>
+     <section className="programmsContainer">
       {
-        programs.map((program)=><ProgramCard program={program} key={program.offer_id}/>)
+        programs.map((program)=><ProgramCard program={program} playerId={playerName} key={program.offer_id}/>)
       }
-     </div>
+     </section>
+     <secion className="info">
+      NOTE: You won't get any reward for playing these games or going to the offer page.<br/>
+      This is just an inventory of the programs I recommend 😊
+
+     </secion>
     </div>
   );
 }
